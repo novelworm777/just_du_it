6e45b646a647424897408d2da9f48902395b9c8f
Mikhaya <mikhaya.jrh@gmail.com>
2020-12-13 00:46:33 +0700

    base template

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -111,7 +111,7 @@ class AuthController extends Controller
     public function logout()
     {
         Auth::logout(); // menghapus session yang aktif
-        return redirect()->route('login');
+        return redirect()->route('/');
     }
  
  
--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,17 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', 'AuthController@showFormLogin')->name('login');
-Route::get('login', 'AuthController@showFormLogin')->name('login');
-Route::post('login', 'AuthController@login');
-Route::get('register', 'AuthController@showFormRegister')->name('register');
-Route::post('register', 'AuthController@register');
+Route::get('/', 'Test@test');
+
+Route::get('/login', 'AuthController@showFormLogin')->name('login');
+Route::post('/login', 'AuthController@login');
+Route::get('/register', 'AuthController@showFormRegister')->name('register');
+Route::post('/register', 'AuthController@register');
  
 Route::group(['middleware' => 'auth'], function () {
  
-    Route::get('home', 'HomeController@index')->name('home');
-    Route::get('logout', 'AuthController@logout')->name('logout');
- 
-});
+    Route::get('/home', 'HomeController@index')->name('home'); // nanti diganti ke /
+    Route::get('/logout', 'AuthController@logout')->name('logout');
+    // route pada dropdown di sini smua butuh middleware
 
-Route::get('/', function () {
-    return view('welcome');
 });
-
