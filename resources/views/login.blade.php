260740c39f4456e315fb00527d188bf999df1249
Mikhaya <mikhaya.jrh@gmail.com>
2020-12-20 00:44:28 +0700

    login register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,15 +21,12 @@
                 @csrf
                 <div class="card-body">
                     @if(session('errors'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <div class="alert alert-danger" role="alert">
                             Something is wrong:
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
                             <ul>
-                            @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
                             </ul>
                         </div>
                     @endif
