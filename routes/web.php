19ff24e45c11c204205f4502bc336f3451d5d2b8
Mikhaya <mikhaya.jrh@gmail.com>
2020-12-21 18:07:09 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -15,47 +15,43 @@ use Illuminate\Support\Facades\Route;
 
 // home or view all shoes
 Route::get('/', 'ViewShoeController@viewAllShoe');
-// view cart
-Route::get('/view-cart', 'CartController@viewCart');
-// checkout
-Route::get('/cart-checkout', 'CartController@checkoutCart');
-// view all transaction (admin)
-Route::get('/view-all-transaction', 'viewTransactionController@viewAllTransaction');
-// view transaction (member)
-Route::get('/view-transaction', 'viewTransactionController@viewMemberTransaction');
-
-
-
 // login
 Route::get('/login', 'AuthController@showFormLogin')->name('login');
 Route::post('/login', 'AuthController@login');
 // register
 Route::get('/register', 'AuthController@showFormRegister')->name('register');
 Route::post('/register', 'AuthController@register');
-//  routes that need middleware
-Route::group(['middleware' => 'auth'], function () {
+// logout
+Route::get('/logout', 'AuthController@logout')->middleware('auth');
+// view shoe details
+Route::get('/shoe={id}', 'ViewShoeController@viewShoe');
 
-    // Route::get('/home', 'ViewShoeController@viewAllShoe')->name('home'); // nanti diganti ke /
-    Route::get('/logout', 'AuthController@logout');
-    // route pada dropdown di sini smua butuh middleware
-    // mungkin route nya ga usah dinamain... liat nanti lagi deh
-
+// role : member
+Route::group(['middleware' => ['auth', 'role:member']], function() {
+    // add to cart
+    Route::get('/shoe={id}/add-to-cart', 'CartController@viewAddCart')->name('add_cart');
+    Route::post('/shoe={id}/add-to-cart', 'CartController@addCart');
+    // view cart
+    Route::get('/view-cart', 'CartController@viewCart');
+    // edit cart
+    Route::get('/edit-cart={id}', 'CartController@editCart')->name('edit_cart');
+    Route::post('/edit-cart={id}', 'CartController@updateCart');
+    // delete cart
+    Route::get('/delete-cart={id}', 'CartController@deleteCart');
+    // checkout
+    Route::get('/cart-checkout', 'CartController@checkoutCart');
+    // view transaction
+    Route::get('/view-transaction', 'viewTransactionController@viewMemberTransaction');
 });
 
-// view shoe
-Route::get('/shoe={id}', 'ViewShoeController@viewShoe');
-// add to cart
-Route::get('/shoe={id}/add-to-cart', 'CartController@viewAddCart')->name('add_cart');
-Route::post('/shoe={id}/add-to-cart', 'CartController@addCart');
-// edit cart
-Route::get('/edit-cart={id}', 'CartController@editCart')->name('edit_cart');
-Route::post('/edit-cart={id}', 'CartController@updateCart');
-// delete cart
-Route::get('/delete-cart={id}', 'CartController@deleteCart');
-
-// add shoe
-Route::get('/add-shoe', 'ShoeController@showAddShoe')->name('add_shoe');
-Route::post('/add-shoe', 'ShoeController@addShoe');
-// update shoe
-Route::get('/shoe={id}/edit-shoe', 'ShoeController@editShoe')->name('edit_shoe');
-Route::post('/shoe={id}/edit-shoe', 'ShoeController@updateShoe');
+// role : admin
+Route::group(['middleware' => ['auth', 'role:admin']], function() {
+    // update shoe
+    Route::get('/shoe={id}/edit-shoe', 'ShoeController@editShoe')->name('edit_shoe');
+    Route::post('/shoe={id}/edit-shoe', 'ShoeController@updateShoe');
+    // add shoe
+    Route::get('/add-shoe', 'ShoeController@showAddShoe')->name('add_shoe');
+    Route::post('/add-shoe', 'ShoeController@addShoe');
+    // view all transaction
+    Route::get('/view-all-transaction', 'viewTransactionController@viewAllTransaction');
+});
